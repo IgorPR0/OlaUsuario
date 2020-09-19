146421714e8aed8e08a14be2e5a71e8b6faa8dba
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-18 21:04:53 -0300

    Feito atividade OlaUsuario

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            //Entrada(Digite//
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nome}!");
+            //Final(Digite)//
+
+            //Partida(Sair)//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //final(Sair)//
+        }
+    }
+}
